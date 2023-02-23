0bc163a5addd044b9e6a31acf314fced83ca7fc9
Sadiyahk25 <95752277+Sadiyahk25@users.noreply.github.com>
2023-02-23 22:33:43 +0400

    modified

--- main.js
+++ main.js
@@ -3,5 +3,17 @@ let collapse=document.querySelectorAll("#header .collapse");
 
 toggle.addEventListener('click',function(){
     collapse.forEach(col=>col.classList.toggle("collapse-toggle"));
-    
 })
+
+var icon = document.getElementById("moon-icon");
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    document.body.classList.toggle("dark-theme hr-line");
+    document.body.classList.toggle("dark-theme home-content");
+
+    if(document.body.classList.toggle("dark-theme") == true){
+        icon.innerHTML= '<i class="fa-solid fa-sun"></i>';
+    } else {
+        icon.innerHTML='<i class="fa-solid fa-moon"></i>';
+    }
+}
