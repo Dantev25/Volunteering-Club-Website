0ad4e17e7803ae28f6ec347a1294824edf0a9e4c
Mrishana <mrishanar2712@gmail.com>
2023-02-24 22:31:17 +0400

    Added drag and drop feature, 3D flip Feature and the image comparison slider feature.

--- main.js
+++ main.js
@@ -17,3 +17,150 @@ icon.onclick = function(){
         icon.innerHTML='<i class="fa-solid fa-moon"></i>';
     }
 }
+
+//drag and drop
+var diffX, diffY, theElement;
+
+// The event handler function for grabbing the word
+
+function grabber(event) {
+
+// Set the global variable for the element to be moved
+
+  theElement = event.currentTarget;
+
+// Determine the position of the word to be grabbed,
+//  first removing the units from left and top
+
+  var posX = parseInt(theElement.style.left);
+  var posY = parseInt(theElement.style.top);
+
+// Compute the difference between where it is and 
+//  where the mouse click occurred
+
+  diffX = event.clientX - posX;
+  diffY = event.clientY - posY;
+
+// Now register the event handlers for moving and 
+//  dropping the word
+ 
+  document.addEventListener("mousemove", mover, true);
+  document.addEventListener("mouseup", dropper, true);
+
+// Stop propagation of the event and stop any default 
+//  browser action
+
+  event.stopPropagation();
+  event.preventDefault();
+
+}  //** end of grabber
+
+// *******************************************************
+
+// The event handler function for moving the word
+
+function mover(event) {
+
+// Compute the new position, add the units, and move the word
+
+  theElement.style.left = (event.clientX - diffX) + "px";
+  theElement.style.top = (event.clientY - diffY) + "px";
+
+// Prevent propagation of the event
+
+  event.stopPropagation();
+}  //** end of mover
+
+// *********************************************************
+// The event handler function for dropping the word
+
+function dropper(event) {
+
+// Unregister the event handlers for mouseup and mousemove
+
+  document.removeEventListener("mouseup", dropper, true);
+  document.removeEventListener("mousemove", mover, true);
+
+// Prevent propagation of the event
+
+  event.stopPropagation();
+}  //** end of dropper
+
+/////////////////////////
+//comparison slide
+function initComparisons() {
+    var x, i;
+    /*find all elements with an "overlay" class:*/
+    x = document.getElementsByClassName("img-comp-overlay");
+    for (i = 0; i < x.length; i++) {
+      /*once for each "overlay" element:
+      pass the "overlay" element as a parameter when executing the compareImages function:*/
+      compareImages(x[i]);
+    }
+    function compareImages(img) {
+      var slider, img, clicked = 0, w, h;
+      /*get the width and height of the img element*/
+      w = img.offsetWidth;
+      h = img.offsetHeight;
+      /*set the width of the img element to 50%:*/
+      img.style.width = (w / 2) + "px";
+      /*create slider:*/
+      slider = document.createElement("DIV");
+      slider.setAttribute("class", "img-comp-slider");
+      /*insert slider*/
+      img.parentElement.insertBefore(slider, img);
+      /*position the slider in the middle:*/
+      slider.style.top = (h / 2) - (slider.offsetHeight / 2) + "px";
+      slider.style.left = (w / 2) - (slider.offsetWidth / 2) + "px";
+      /*execute a function when the mouse button is pressed:*/
+      slider.addEventListener("mousedown", slideReady);
+      /*and another function when the mouse button is released:*/
+      window.addEventListener("mouseup", slideFinish);
+      /*or touched (for touch screens:*/
+      slider.addEventListener("touchstart", slideReady);
+      /*and released (for touch screens:*/
+      window.addEventListener("touchend", slideFinish);
+      function slideReady(e) {
+        /*prevent any other actions that may occur when moving over the image:*/
+        e.preventDefault();
+        /*the slider is now clicked and ready to move:*/
+        clicked = 1;
+        /*execute a function when the slider is moved:*/
+        window.addEventListener("mousemove", slideMove);
+        window.addEventListener("touchmove", slideMove);
+      }
+      function slideFinish() {
+        /*the slider is no longer clicked:*/
+        clicked = 0;
+      }
+      function slideMove(e) {
+        var pos;
+        /*if the slider is no longer clicked, exit this function:*/
+        if (clicked == 0) return false;
+        /*get the cursor's x position:*/
+        pos = getCursorPos(e)
+        /*prevent the slider from being positioned outside the image:*/
+        if (pos < 0) pos = 0;
+        if (pos > w) pos = w;
+        /*execute a function that will resize the overlay image according to the cursor:*/
+        slide(pos);
+      }
+      function getCursorPos(e) {
+        var a, x = 0;
+        e = (e.changedTouches) ? e.changedTouches[0] : e;
+        /*get the x positions of the image:*/
+        a = img.getBoundingClientRect();
+        /*calculate the cursor's x coordinate, relative to the image:*/
+        x = e.pageX - a.left;
+        /*consider any page scrolling:*/
+        x = x - window.pageXOffset;
+        return x;
+      }
+      function slide(x) {
+        /*resize the image:*/
+        img.style.width = x + "px";
+        /*position the slider:*/
+        slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + "px";
+      }
+    }
+  }
