cef64a80652f983181a37a75a72019aebe2bab3f
Sadiyahk25 <95752277+Sadiyahk25@users.noreply.github.com>
2023-02-24 21:36:45 +0400

    Modified homepage

--- main.js
+++ main.js
@@ -8,12 +8,18 @@ toggle.addEventListener('click',function(){
 var icon = document.getElementById("moon-icon");
 icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
-    document.body.classList.toggle("dark-theme hr-line");
-    document.body.classList.toggle("dark-theme home-content");
+    // document.body.classList.toggle("dark-theme hr-line");
+    // document.body.classList.toggle("dark-theme home-content");
+    // document.body.classList.toggle("dark-theme h1");
+    // document.body.classList.toggle("dark-theme article-bg");
+    document.body.classList.toggle("dark-theme dark-logo");
+    // document.body.classList.toggle("dark-theme recent-articles p");
 
-    if(document.body.classList.toggle("dark-theme") == true){
+    if(document.body.classList.toggle("dark-theme")){
         icon.innerHTML= '<i class="fa-solid fa-sun"></i>';
     } else {
         icon.innerHTML='<i class="fa-solid fa-moon"></i>';
     }
+
 }
+
